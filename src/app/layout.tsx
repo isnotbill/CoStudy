f3bfe824f549ca14a806493beb2bc2c4d0d06b16
Rei <reicareich@gmail.com>
2025-05-25 15:18:59 -0500

    Change to Next.js Add Header component Create Home page

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,15 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
+import { Cedarville_Cursive } from "next/font/google";
 import "./globals.css";
+
+import Header from "@/components/Header";
+
+const cedarville = Cedarville_Cursive({
+  weight: '400',
+  variable: '--font-cedarville',
+  subsets: ["latin"]
+});
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -11,6 +20,8 @@ const geistMono = Geist_Mono({
   variable: "--font-geist-mono",
   subsets: ["latin"],
 });
+
+
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -24,9 +35,11 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
+        className={`${geistSans.variable} ${geistMono.variable} ${cedarville.variable} antialiased`}
+        >
+        
         {children}
       </body>
     </html>
---
+++ src/components/Header.tsx
@@ -0,0 +1,34 @@
+import Link from 'next/link';
+import Head from 'next/head';
+
+const Header = () => {
+  return (
+    <>
+    <header className="flex justify-center items-center p-[5px] text-[rgb(242,247,253)] bg-[rgb(70,60,102)]">
+      <h1 className="font-cedarville font-thin text-[45px] mx-[80px]">
+        costudy
+      </h1>
+
+      <div className="flex items-center gap-[80px] font-kumbh font-medium text-[10px] mr-[100px]">
+        <Link href="/" className="text-[#dfebf7] text-[20px] no-underline hover:text-[#d6d6d6] hover:underline">
+          Home
+        </Link>
+
+        <Link href="/about" className="text-[#dfebf7] text-[20px] no-underline hover:text-[#d6d6d6] hover:underline">
+          About Us
+        </Link>
+
+        <Link href="/registration" passHref>
+          <button
+            className="border-none bg-[rgb(244,249,253)] text-[rgb(72,23,112)] px-[40px] py-1 rounded-[30px] font-kumbh font-normal text-[20px] cursor-pointer transition-colors duration-200 hover:bg-[#d4d4d4]"
+          >
+            Login
+          </button>
+        </Link>
+      </div>
+    </header>
+        </>
+  );
+};
+
+export default Header;
