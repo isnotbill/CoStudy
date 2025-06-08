20fb0042b6381db4a04dadc21ec75fb0e35cc9ab
isnotbill <billh0375@gmail.com>
2025-06-08 18:31:36 -0400

    Connect /register API to frontend

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,7 +1,43 @@
-import Image from "next/image";
+'use client'
+
+import axios from 'axios'
+import Image from "next/image"
 import Header from "@/components/Header"
+import { useState } from 'react';
 
 export default function Home() {
+  const [error, setError] = useState<string | null>(null);
+  // console.log("hello");
+  async function handleSubmit(e: React.FormEvent){
+    e.preventDefault()
+    setError(null)
+    const form = new FormData(e.currentTarget as HTMLFormElement)
+    const confirmPassword = form.get("confirm")
+    const payload = {
+      email: form.get("email"),
+      username: form.get("username"),
+      password: form.get("password")
+    }
+
+    if (payload.password != confirmPassword) {
+      setError("Passwords don't match!")
+      return
+    }
+
+    try {
+      const { data } = await axios.post(
+        'http://localhost:8080/register',
+        payload,
+        { timeout: 5000 }
+      )
+      // window.location.href='/home'
+      console.log(data);
+
+    } catch (error: any) {
+      setError(error.message)
+    }
+  }
+
   return (
     <main className="h-screen flex flex-col select-none">
       <Header/>
@@ -17,15 +53,32 @@ export default function Home() {
             height={800}
           />
         </div>
-        <form className="bg-gray-100 w-full max-w-[400px] min-w-[300px] h-[500px] px-[20] gap-8 rounded-2xl flex flex-col justify-center items-center">
+        <form 
+        onSubmit={handleSubmit}
+        className="bg-gray-100 w-full max-w-[400px] min-w-[300px] h-[500px] px-[20] gap-8 rounded-2xl flex flex-col justify-center items-center">
           <h2 className="text-2xl font-bold text-[rgba(49,32,77,0.8)] text-center">Create Your Account</h2>
 
           <div className="flex flex-col gap-2 w-full">
+            <div className="flex flex-col">
+              <label htmlFor="confirm-password" className="text-[rgba(49,32,77,0.8)] font-medium">
+                Email
+              </label>
+              <input
+                name="email"
+                type="email"
+                id="confirm-password"
+                placeholder="Your email"
+                required
+                className="w-full px-4 py-2 border border-gray-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-indigo-500"
+                minLength={8}
+              />
+            </div>
             <div className="flex flex-col">
               <label htmlFor="username" className="text-[rgba(49,32,77,0.8)] font-medium">
                 Username
               </label>
               <input
+                name="username"
                 type="text"
                 id="username"
                 placeholder="Your username"
@@ -39,6 +92,7 @@ export default function Home() {
                 Password
               </label>
               <input
+                name="password"
                 type="password"
                 id="password"
                 placeholder="Enter password"
@@ -53,6 +107,7 @@ export default function Home() {
                 Confirm Password
               </label>
               <input
+                name="confirm"
                 type="password"
                 id="confirm-password"
                 placeholder="Confirm password"
@@ -61,6 +116,7 @@ export default function Home() {
                 minLength={8}
               />
             </div>
+            {error && <p className="text-red-500">{error}</p>}
           </div>
           <button
             type="submit"
