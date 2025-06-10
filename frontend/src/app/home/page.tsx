1338d637e7fd7a6507222531018111ae520af23f
isnotbill <billh0375@gmail.com>
2025-06-09 20:25:30 -0400

    Implement middleware routing

--- frontend/src/app/home/page.tsx
+++ frontend/src/app/home/page.tsx
@@ -1,7 +1,33 @@
+'use client'
+
 import Image from 'next/image'
 import MainHeader from '@/components/MainHeader'
+import axios from 'axios'
+import { useEffect, useState } from 'react'
 
 export default function HomePage(){
+    const [profile, setProfile] = useState<any>(null)
+    const [error, setError] = useState<string | null>(null)
+    useEffect(()=> {
+        async function fetchProfile(){
+            try {
+                const res = await axios.get('http://localhost:8080/user',
+                     { withCredentials: true, timeout: 5000 }
+                    )
+                setProfile(res.data)    
+            } catch (err: any){
+                const msg = err.message
+                setError(msg)
+                console.log("hello")
+                console.log(msg)
+            }
+        }
+        fetchProfile()
+
+    }, [])
+
+    //if (error){window.location.href='/login'}
+    
     return (
         <>
         <main className='bg-[rgb(53,46,78)] w-screen h-screen flex flex-col items-center'>
@@ -11,7 +37,7 @@ export default function HomePage(){
                     <div className="flex-none border-8 border-[rgb(70,60,102)]  bg-white w-min-[200px] w-[200px] min-h-[200px] h-[200px] rounded-full">
 
                     </div>
-                    <h1 className="text-white text-3xl">Username</h1>
+                    <h1 className="text-white text-3xl">{profile?.username}</h1>
                 </div>
                 <div className='w-[1000] h-full flex gap-4'>
                     <div className='bg-[rgb(70,60,102)] w-[500] h-full rounded-3xl'>
---
+++ frontend/src/middleware.ts
@@ -0,0 +1,35 @@
+import { NextResponse } from "next/server";
+import type { NextRequest } from "next/server"
+
+export const config = {
+  matcher: '/:path*',  
+}
+
+export function middleware(req: NextRequest){
+
+  const { pathname } = req.nextUrl
+
+    if (
+    pathname.startsWith('/_next') ||
+    pathname.startsWith('/favicon.ico') ||
+    pathname.startsWith('/images') 
+  ) {
+    return NextResponse.next()
+  }
+
+  console.log('[MW] middleware fired for:', req.nextUrl.pathname)
+  
+  if (['/login', '/'].includes(req.nextUrl.pathname)){
+    return NextResponse.next()
+  }
+
+  const token = req.cookies.get('token')?.value
+  
+  if (!token){
+    const url = req.nextUrl.clone()
+    url.pathname = '/login'
+    return NextResponse.redirect(url)
+  } else {
+    return NextResponse.next()
+  }
+}
