a1c7ab53d86a31c32d302d28dbd762de4d67ee5a
Rei <reicareich@gmail.com>
2025-07-20 23:42:23 -0500

    Fix S3 avatar upload handling

--- backend/src/main/java/org/costudy/backend/controller/S3Controller.java
+++ backend/src/main/java/org/costudy/backend/controller/S3Controller.java
@@ -1,5 +1,6 @@
 package org.costudy.backend.controller;
 
+import org.costudy.backend.model.User;
 import org.costudy.backend.service.S3Service;
 import org.costudy.backend.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -13,6 +14,7 @@ import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.io.IOException;
+import java.util.Map;
 
 @RestController
 public class S3Controller {
@@ -26,11 +28,18 @@ public class S3Controller {
     }
 
     @PostMapping("/api/{id}/upload")
-    public ResponseEntity<String> upload(
+    public ResponseEntity<Map<String, String>> upload(
             @RequestParam("file") MultipartFile file,
             @PathVariable int id
             ) throws IOException {
         String key = s3Service.uploadFile(file, id);
-        return ResponseEntity.ok("File uploaded successfully");
+        User user = userService.getCurrentUserById(id);
+
+        user.setImage(key);
+        userService.save(user);
+
+        System.out.println();
+
+        return ResponseEntity.ok(Map.of("avatarUrl", key));
     }
 }
--- frontend/src/components/PublicProfile.tsx
+++ frontend/src/components/PublicProfile.tsx
@@ -23,7 +23,7 @@ export default function PublicProfile( { user } : Profile ) {
   const [src, setSrc] = useState('http://localhost:8080/default-avatar.png')
 
   useEffect(() => {
-    if (user.image != null){setSrc(`http://localhost:8080/avatars/${user.image}`)}   
+    if (user.image != null){setSrc(`https://costudy-images-bucket.s3.ca-central-1.amazonaws.com/${user.image}?=${Date.now()}`)}   
 
   }, [user.image])
 
@@ -42,7 +42,7 @@ export default function PublicProfile( { user } : Profile ) {
 
     try {
       const res = await apiClient.post(
-        `http://localhost:8080/${profile.id}/avatar`,
+        `http://localhost:8080/api/${profile.id}/upload`,
         formData,
         {
           withCredentials: true,
@@ -51,7 +51,7 @@ export default function PublicProfile( { user } : Profile ) {
         }
       )
 
-      setSrc(`http://localhost:8080/avatars/${res.data.avatarUrl}`)
+      setSrc(`https://costudy-images-bucket.s3.ca-central-1.amazonaws.com/${res.data.avatarUrl}?=${Date.now()}`)
 
     } catch (err: any) {
 
