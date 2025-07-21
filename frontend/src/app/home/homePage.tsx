650db459705b29451f633f21cf5f3b89bc3318ff
Rei <reicareich@gmail.com>
2025-07-20 23:14:08 -0500

    Update S3 display and upload on home and settings

--- frontend/src/app/home/homePage.tsx
+++ frontend/src/app/home/homePage.tsx
@@ -36,7 +36,7 @@ export default function HomePage() {
 
   let src = 'http://localhost:8080/avatars/default-avatar.png'
   if (profile?.image) {
-    src = `http://localhost:8080/avatars/${profile.image}`
+    src = `https://costudy-images-bucket.s3.ca-central-1.amazonaws.com/${profile.image}?=${Date.now()}`
   }
 
   return (
