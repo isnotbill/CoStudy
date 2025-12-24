64ecb0343b4efbc12245fecdf484967d0ebd1039
Rei <reicareich@gmail.com>
2025-12-24 01:04:46 -0600

    Change theme

---
+++ ui/src/app/home/page.tsx
@@ -0,0 +1,17 @@
+import DotGridBackground from "@/components/ui/background"
+import { Header } from "@/components/header"
+
+export default function Home() {
+  return (
+    <>
+      <Header />
+      <section className="relative flex min-h-screen w-full items-center justify-center overflow-hidden bg-glow-mesh pt-20 pb-5">
+        <div className="absolute inset-0 z-0 from-[#513d9b]">
+          <DotGridBackground />
+        </div>
+
+
+      </section>
+    </>
+  )
+}
--- ui/tailwind.config.ts
+++ ui/tailwind.config.ts
@@ -14,6 +14,16 @@ export default {
       },
       fontFamily: {
         cedarville: ['var(--font-cedarville)']
+      },
+      animation: {
+        'pulse-slow': 'pulse 6s cubic-bezier(0.4, 0, 0.6, 1) infinite',
+        'float': 'float 3s ease-in-out infinite',
+      },
+      keyframes: {
+        float: {
+          '0%, 100%': { transform: 'translateY(0)' },
+          '50%': { transform: 'translateY(-10px)' },
+        }
       }
     },
   },
