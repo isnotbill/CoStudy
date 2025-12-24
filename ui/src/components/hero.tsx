b6f0a5f14ec3e826e139df14a096d16b1c67821b
Rei <reicareich@gmail.com>
2025-12-24 01:04:46 -0600

    Change theme

--- ui/src/components/hero.tsx
+++ ui/src/components/hero.tsx
@@ -3,24 +3,13 @@
 import { Button } from "@/components/ui/button";
 import Image from "next/image";
 import { SignupForm } from "./signup-form";
-import { BackgroundGradientAnimation } from "./ui/background-gradient-animation";
+import DotGridBackground from "./ui/background"
 
 export const Hero = () => {
   return (
       <section className="relative flex min-h-screen w-full items-center justify-center overflow-hidden bg-glow-mesh pt-20 pb-5">
         <div className="absolute inset-0 z-0 from-[#513d9b]">
-          <BackgroundGradientAnimation 
-            firstColor="118, 82, 156"
-            secondColor="118, 82, 160" 
-            thirdColor="120, 82, 156" 
-            fourthColor="118, 82, 170"
-            fifthColor="118, 82, 156"
-            pointerColor=""
-            gradientBackgroundEnd="rgb(90, 50, 120)"
-            gradientBackgroundStart="#3c2f69"
-            size="90%"
-            containerClassName="h-full w-full"
-            />
+          <DotGridBackground />
         </div>
 
         <div className="container px-4 md:px-6 z-10">
@@ -45,7 +34,7 @@ export const Hero = () => {
                   width={0}
                   height={0}
                   sizes="100vw"
-                  className="w-full max-w-md sm:max-w-lg h-auto"
+                  className="w-full max-w-md sm:max-w-lg h-auto drop-shfadow-[0_1px_48px_rgba(120,140,255,0.4)]"
                   />
             </div>
 
